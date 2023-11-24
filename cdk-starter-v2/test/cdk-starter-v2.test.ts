aacd94fa1c765eb1f8a521e08897b1f5cd510493
Antony Chiramel <47243179+5thComrade@users.noreply.github.com>
2023-11-24 15:11:51 +0000

    cdk starter v2 added with the cdk cli version matching cloud terminal

---
+++ cdk-starter-v2/test/cdk-starter-v2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkStarterV2 from '../lib/cdk-starter-v2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-starter-v2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkStarterV2.CdkStarterV2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
